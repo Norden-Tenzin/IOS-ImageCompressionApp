168c800810d0acca8735a86fb164768ea432b92d
Tenzin Norden <nordentenzin29@gmail.com>
2023-04-15 18:07:48 +0530

    Initial Commit

---
+++ ImageCompress/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ImageCompress
+//
+//  Created by Tenzin Norden on 4/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageCompress/ImageCompressApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCompressApp.swift
+//  ImageCompress
+//
+//  Created by Tenzin Norden on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageCompressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
